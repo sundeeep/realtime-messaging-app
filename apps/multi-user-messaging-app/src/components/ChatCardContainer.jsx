4531ae1a897e44ae37e50440190981b6cbed4775
sundeeep <sandeepkumar25820@gmail.com>
2024-08-16 22:51:16 +0530

    [frontend] Chat UI v1 completed

---
+++ apps/multi-user-messaging-app/src/components/ChatCardContainer.jsx
@@ -0,0 +1,13 @@
+import ChatCard from "./reusable/ChatCard";
+
+const ChatCardContainer = () => {
+  return (
+    <div className="flex flex-col items-stretch flex-1 w-full overflow-y-auto">
+        {Array(20).fill(null).map(()=>(
+            <ChatCard name='Sundeeep Dasari' lastSeen={2} lastText='Your code will only be used to evaluate you, and will not be used in any production application. We look forward to seeing your work, have a great day! 😁'/>
+        ))}
+    </div>
+  )
+}
+
+export default ChatCardContainer;
---
+++ apps/multi-user-messaging-app/src/components/ui/Avatar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Avatar = () => {
+  return (
+    <div className="h-[60px] w-[60px] rounded-full">
+        <img className="h-full w-full rounded-full object-cover" src="https://xsgames.co/randomusers/avatar.php?g=male" alt="" />
+    </div>
+  )
+}
+
+export default Avatar
