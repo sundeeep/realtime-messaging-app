ed72f7678c5bac99d64889ac652e1cd0d081b2b7
sundeeep <sandeepkumar25820@gmail.com>
2024-11-25 21:08:19 +0530

    chat card container tweaked

--- apps/multi-user-messaging-app/src/components/ChatCardContainer.jsx
+++ apps/multi-user-messaging-app/src/components/ChatCardContainer.jsx
@@ -3,8 +3,8 @@ import ChatCard from "./reusable/ChatCard";
 const ChatCardContainer = () => {
   return (
     <div className="flex flex-col items-stretch flex-1 w-full overflow-y-auto">
-        {Array(20).fill(null).map(()=>(
-            <ChatCard isLastMessageSentByYou={true} gender={"male"} name='Sundeeep Dasari' lastSeen={2} lastText='Your code will only be used to evaluate you, and will not be used in any production application. We look forward to seeing your work, have a great day! 😁'/>
+        {Array(20).fill(null).map((_, index)=>(
+            <ChatCard key={index} isLastMessageSentByYou={true} gender={"male"} name='Sundeeep Dasari' lastSeen={2} lastText='Your code will only be used to evaluate you, and will not be used in any production application. We look forward to seeing your work, have a great day! 😁'/>
         ))}
     </div>
   )
