0b06e46cd3fff388360cc8afc2b9f3bd1438864c
sundeeep <sandeepkumar25820@gmail.com>
2024-11-25 21:08:00 +0530

    Authentication state hook added

--- apps/multi-user-messaging-app/src/store/useAuthStore.js
+++ apps/multi-user-messaging-app/src/store/useAuthStore.js
@@ -1,9 +1,27 @@
 import {create} from 'zustand';
+import io from 'socket.io-client';
 
 const useAuthStore = create((set) => ({
   user: null,
   setUser: (user) => set({ user }),
   logout: () => set({ user: null }),
+  registerNewUser: async (newUser) => {
+    try{
+      const socket = io('http://localhost:3500');
+      if(newUser){
+        socket.emit('register-new-user', newUser);
+      }
+    //   socket.on('new-user-registered', (newUser) => {
+    //     if(newUser){
+    //       set({ user: newUser });
+    //       console.log('New user registered', newUser);
+    //     }
+    // });
+    }catch(error){
+      console.error("Error registering user", error.message);
+      return null;
+    }
+  },
   login: async (username, password) => {
     try {
       const response = await fetch('http://localhost:3000/api/login', {
